9e6f8b2281aaef65403bb9fe799ff091c6aae838
AnneTjula <88149819+AnneTjula@users.noreply.github.com>
2021-08-30 16:17:55 +0200

    Add files via upload

---
+++ wall.js
@@ -0,0 +1,38 @@
+const name = "Anne";
+console.log(name);
+
+/* const paint = function ()) {console.log ("I'm painting" 
++ item + "red" );}
+const wall= "red";
+// paint (surface); */
+
+/* const paint = function (color) { console.log("The wall has been painted " + color) };
+const surface = "wall"
+const color = "red"
+paint(surface);
+paint(color);
+ */
+/* console.log("the wall has been painted red");
+
+const paint = function (wall, color) { console.log(" You need to paint the " + wall + color) };
+const surface = "wall"
+const color = "red"
+paint(surface, color); */
+
+/* console.log("the wall has been painted red");
+
+const paint = function (wall, color) { console.log(" You need to paint the " + wall + color) };
+const surface = "wall"
+const color = "green"
+paint(surface, color);
+
+console.log("the wall has been painted red");
+ */
+const paint = function (location, color) { console.log("The " + location + " has been painted " + color) };
+const location = "northwall"
+const color = "orange"
+paint(location, color);
+
+
+
+
